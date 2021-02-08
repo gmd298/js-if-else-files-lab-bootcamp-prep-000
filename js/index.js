4dc9425a10136b080503ff690591a0a3e7ab9623
decisive-metric-0348 <gmd298@gmail.com>
2021-02-08 17:49:12 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 var name = "Susan"
+<<<<<<< HEAD
 var height = "70"
+=======
+var height = "70,"
+>>>>>>> ba0b10ba5d619b0e38b4553825f070c577a37115
  
 // Don't worry about this
 module.exports = { name, height
